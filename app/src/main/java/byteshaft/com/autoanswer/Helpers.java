9b76094d866ca11a0811c886de90f5efb99b7b15
Omer Akram <om26er@gmail.com>
2015-05-05 16:21:53 +0500

    Add helpers class

---
+++ app/src/main/java/byteshaft/com/autoanswer/Helpers.java
@@ -0,0 +1,25 @@
+package byteshaft.com.autoanswer;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.view.KeyEvent;
+
+public class Helpers extends ContextWrapper {
+
+    public Helpers(Context base) {
+        super(base);
+    }
+
+    TelephonyManager getTelephonyManager() {
+        return (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+    }
+
+    void pickUpCall() {
+        Intent intent = new Intent(Intent.ACTION_MEDIA_BUTTON);
+        intent.putExtra(Intent.EXTRA_KEY_EVENT, new KeyEvent(
+                KeyEvent.ACTION_UP, KeyEvent.KEYCODE_HEADSETHOOK));
+        sendOrderedBroadcast(intent, null);
+    }
+}
--- app/src/main/java/byteshaft/com/autoanswer/SensorService.java
+++ app/src/main/java/byteshaft/com/autoanswer/SensorService.java
@@ -9,49 +9,35 @@ import android.hardware.SensorManager;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
-import android.view.KeyEvent;
 
 
 public class SensorService extends Service implements SensorEventListener {
-    CallStateListener callStateListener;
 
-    private Sensor proximitySensor;
-
+    private CallStateListener mCallStateListener;
+    private Sensor mProximitySensor;
+    private Helpers mHelpers;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
+        mHelpers = new Helpers(getApplicationContext());
         SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-        sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_UI);
-        callStateListener = new CallStateListener();
-        TelephonyManager telephonyManager = getTelephonyManager();
-        telephonyManager.listen(callStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+        mProximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        sensorManager.registerListener(this, mProximitySensor, SensorManager.SENSOR_DELAY_UI);
+        mCallStateListener = new CallStateListener();
+        TelephonyManager telephonyManager = mHelpers.getTelephonyManager();
+        telephonyManager.listen(mCallStateListener, PhoneStateListener.LISTEN_CALL_STATE);
         return START_STICKY;
     }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
+    @Override public IBinder onBind(Intent intent) { return null; }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.values[0] != proximitySensor.getMaximumRange() && callStateListener.isCallIncoming()) {
-            Intent intent = new Intent(Intent.ACTION_MEDIA_BUTTON);
-            intent.putExtra(Intent.EXTRA_KEY_EVENT, new KeyEvent(
-                    KeyEvent.ACTION_UP, KeyEvent.KEYCODE_HEADSETHOOK));
-            sendOrderedBroadcast(intent, null);
-            System.out.println("ok");
+        if (event.values[0] != mProximitySensor.getMaximumRange() &&
+                mCallStateListener.isCallIncoming()) {
+            mHelpers.pickUpCall();
         }
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
-    private TelephonyManager getTelephonyManager() {
-        return (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-    }
+    @Override public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 }
